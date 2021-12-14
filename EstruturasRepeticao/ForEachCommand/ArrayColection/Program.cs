cdc8cfac6fab014b6d82e1ec401bede2f1f097e7
AnaMorata8642 <anacarolinasantosmorata@gmail.com>
2021-12-14 19:47:23 -0300

    commit inicial

---
+++ EstruturasRepeticao/ForEachCommand/ArrayColection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayColection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SINTAXE DO ARRAY: TiposdeDados [] = new TiposdeDados [tamanho];
+
+            string[] alunosAMS = new string[5]
+            {"Ana Julia",
+            "Evelyn Talita",
+            "Gabriel Ramos",
+            "Antonio",
+            "Pedro Pivetta"};
+
+            Console.WriteLine(alunosAMS[2]);
+
+            //alunosAMS[2] = "Gabriel Ramos"
+
+            Console.WriteLine($"Novo valor no index: {alunosAMS[2]}");
+
+            foreach (string alunos in alunosAMS)
+            {
+                Console.WriteLine(alunos);
+            }
+        }
+    }
+}
--- EstruturasRepeticao/ForEachCommand/ArrayColection/README.md
+++ EstruturasRepeticao/ForEachCommand/ArrayColection/README.md
@@ -1,2 +1,7 @@
 # Comandos do git
 1- git init - (comando para inicializar o git)
+2- git remote add origin https://github.com/AnaMorata8642/Estrutura-Repeticao-Array-8642.git
+3- git remote -v (comando para visualizar o repositorio atual)
+4- git commit -m "nome do commit" (commit pacote de alterações)
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
